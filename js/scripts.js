0d0d4b74283932a263d4bfe398ddb61344b898cd
Jessie Guilford <Jessie Guilford>
2016-01-22 09:07:15 -0800

    add 3rd and 4th specs and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 var pingPong = function(inputNum) {
   var result = ""
   for (var i =1; i <= inputNum; i ++) {
-    if (i % 3 === 0) {
+    if (i % 15 === 0) {
+      n = "pingpong";
+    } else if (i % 3 === 0) {
       n = "ping";
+    } else if (i % 5 === 0) {
+      n = "pong";
     } else {
       var n = i;
     };
--- specs/specs.js
+++ specs/specs.js
@@ -3,6 +3,12 @@ describe("pingPong", function() {
     expect(pingPong("2")).to.equal("12");
   });
   it("replaces numbers divisible by 3 with ping", function() {
-    expect(pingPong("5")).to.equal("12ping45");
+    expect(pingPong("4")).to.equal("12ping4");
+  });
+  it("also replaces numbers divisible by 5 with pong", function() {
+    expect(pingPong("10")).to.equal("12ping4pongping78pingpong");
+  });
+  it("also replaces numbers divisible by 15 with pingpong", function() {
+    expect(pingPong("15")).to.equal("12ping4pongping78pingpong11ping1314pingpong");
   });
 });
